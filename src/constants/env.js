9a9103150956fdf6202779d3102a6cca44d4a470
Chetan Wagh <chetan.wagh@oaktreecloud.com>
2020-04-09 18:32:21 +0530

    default image set

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,4 +1,5 @@
 
 
-export const REACT_API_URL = 'http://14b4f6e1.ngrok.io'
+export const REACT_API_URL = 'https://linkedin-fe.herokuapp.com/'
+// export const REACT_API_URL = 'http://14b4f6e1.ngrok.io'
 
