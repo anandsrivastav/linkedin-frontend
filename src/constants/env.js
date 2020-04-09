7941f431290ddae5d36d8f79ad0a3f973f86a488
Chetan Wagh <chetan.wagh@oaktreecloud.com>
2020-04-09 18:46:56 +0530

    url change

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,5 +1,5 @@
 
 
-export const REACT_API_URL = 'https://linkedin-backend.herokuapp.com'
+export const REACT_API_URL = 'https://linkedin-campaign.herokuapp.com'
 // export const REACT_API_URL = 'http://14b4f6e1.ngrok.io'
 
