38632511c9d052edeba65e14a7368835993be7e2
Anand Shrivastava <anand.srivastav@oaktreecloud.com>
2020-04-16 20:35:05 +0530

    Update env.js

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,5 +1,2 @@
-
-
-export const REACT_API_URL = 'https://linkedin-campaign.herokuapp.com'
-// export const REACT_API_URL = 'http://15cf148c.ngrok.io'
+export const REACT_API_URL = 'http://52.90.45.233/'
 
